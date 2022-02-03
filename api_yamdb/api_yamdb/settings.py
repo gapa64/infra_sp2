93b28fa143e145b6f9efb643642bbda6c6d5e501
gapa64 <kurdyukovsn@gmail.com>
2022-02-04 00:58:29 +0100

    first working release pep1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -100,8 +100,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-
 AUTH_USER_MODEL = 'users.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
